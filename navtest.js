2a590d9b8382705e6ed6d445204c5d10af7a6a0f
Emiliakazoo <emiliakazoo@gmail.com>
2014-02-04 13:13:44 -0800

    Initial upload of javascript nav
    This completely sucks.  I am just testing to see if this works.  Amber,
    this here code is ugly I have no style.  Story of my life

---
+++ navtest.js
@@ -0,0 +1,46 @@
+﻿var nerdyDevs =
+[
+{
+    name: "Kala",
+    pets: ["Kitty", "Brown Dog"],
+},
+{
+    name: "Amber",
+    pets: ["Cat", "Red Dog"],
+},
+{
+    name: "Norm",
+    pets: ["Cats", "Catz", "CATS"],
+},
+{
+    name: "Carolyn",
+    pets: ["Cats", "Pony", "Brony"],
+},
+{
+    name: "Emilia",
+    pets: ["More Cats", "Even More"],
+},
+{
+    name: "Alla",
+    pets: ["Parrot"],
+},
+
+
+
+];
+
+$(document).ready(function () {
+    var i = 0;
+    var j = 0;
+    for (i = 0; i < nerdyDevs.length; i++) {
+        $superNerds = nerdyDevs[i];
+        $nerdyDev = nerdyDevs[i].name;
+        for (j = 0; j < $superNerds.pets.length; j++) {
+            $nerdyPet = $superNerds.pets;
+            $petItem = "<li class='petNerds'>" + $nerdyPet + "</li>";
+        }
+        $navItem = "<li class='nerds'>" + $nerdyDev + "<ul class='nerdPets'>" + $petItem + "</ul></li>";
+        $(".nerdNav").append($navItem);
+    }
+});
+//why won't this work???  I have made a huge mistake.
